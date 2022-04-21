e710d1fcb376c6fcd499ff0e64cfac128f1b9505
emmafils <imoroug@gmail.com>
2022-04-21 01:16:57 +0100

    Réponse step2

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ManyocoApp
   class Application < Rails::Application
+    config.time_zone = 'GMT'
+    config.active_record.default_timezone = :local
+    I18n.available_locales = [:en, :fr]
+    config.i18n.default_locale = :fr
 
     config.load_defaults 6.0
 
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe 'Task management function', type: :system do
     context 'When creating a new task' do
       it 'The created task is displayed' do
         visit new_task_path
-      fill_in "Title", with: 'title test'
-      fill_in "Content", with: 'content test'
-      fill_in "Deadline", with: '2021/1/1'
-      select 'started'
-      select 'low'
-      click_on 'Create Task'
+          fill_in "Title", with: 'title test'
+          fill_in "Content", with: 'content test'
+          fill_in "Deadline", with: '2021/1/1'
+          select 'started'
+          select 'low'
+        click_on 'Create Task'
       expect(page).to have_content 'title test'
     end
   end
@@ -32,8 +32,26 @@ end
   describe 'Detailed display function' do
      context 'When transitioned to any task details screen' do
        it 'The content of the relevant task is displayed' do
-
+         visit new_task_path
+           fill_in "Title", with: 'title test1'
+           fill_in "Content", with: 'content test'
+           fill_in "Deadline", with: '2021-01-11 10:30 AM'
+           select 'started'
+           select 'low'
+         click_on 'Create Task'
+         expect(page).to have_content 'title test1'
        end
      end
   end
+  # Add additional test content
+describe 'Detailed tasks List' do
+context 'When tasks are arranged in descending order of creation date and time' do
+  # Implement here
+    it 'list tasks sorted in descending order of creation date' do
+      visit tasks_path
+      
+      assert Task.all.order('created_at desc')
+    end
+  end
+ end
 end
